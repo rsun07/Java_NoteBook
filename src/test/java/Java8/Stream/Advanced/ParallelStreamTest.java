72d32bf7364d5481ae83bfc46aa82f5534410701
Xiaoming Sun <rsun2014@gmail.com>
2018-03-13 17:15:35 -0700

    Fork/Join and parallel stream

--- src/test/java/Java8/Stream/Advanced/ParallelStreamTest.java
+++ src/test/java/Java8/Stream/Advanced/ParallelStreamTest.java
@@ -2,8 +2,14 @@ package Java8.Stream.Advanced;
 
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class ParallelStreamTest {
 
@@ -26,5 +32,44 @@ public class ParallelStreamTest {
         intParallelStream.forEach(s->
                     System.out.println(s+" "+Thread.currentThread().getName())
         );
+    }
+
+
+    private static final long CALCULATOR_END_INPUT = 100000000000L;
+
+    @Test
+    public void testForkJoin() {
+        Instant start = Instant.now();
+
+        ForkJoinPool pool = new ForkJoinPool();
+        ForkJoinTask<Long> task = new ForkJoinCalculator(0L, CALCULATOR_END_INPUT);
+        Long sum = pool.invoke(task);
+
+        Instant end = Instant.now();
+        System.out.println(Duration.between(start, end).getNano() / 1000000);
+    }
+
+    @Test
+    public void testTraditionalFor() {
+        Instant start = Instant.now();
+
+        long sum = 0L;
+
+        for (long i = 0; i < CALCULATOR_END_INPUT; i++) {
+            sum += i;
+        }
+
+        Instant end = Instant.now();
+        System.out.println(Duration.between(start, end).getNano() / 1000000);
+    }
+
+    @Test
+    public void testParallelStream() {
+        Instant start = Instant.now();
+
+        LongStream.rangeClosed(0, CALCULATOR_END_INPUT).parallel().reduce(0, Long::sum);
+
+        Instant end = Instant.now();
+        System.out.println(Duration.between(start, end).getNano() / 1000000);
     }
 }
---
+++ src/test/java/RealTimePractice/longTest.java
@@ -0,0 +1,10 @@
+package RealTimePractice;
+
+import org.junit.Test;
+
+public class longTest {
+    @Test
+    public void test() {
+        System.out.println((long)1e10);
+    }
+}
