8ef75a3e86bb8e601a3fa6bb48c1fd6e58b37a03
Xiaoming Sun <rsun2014@gmail.com>
2018-03-13 20:02:05 -0700

    Refactor traditional thread and add callable interface

--- src/test/java/JavaBasicTest/MultiThread/TraditionalTread/TraditionalThread.java
+++ src/test/java/JavaBasicTest/MultiThread/TraditionalTread/TraditionalThread.java
@@ -1,48 +1,76 @@
 package JavaBasicTest.MultiThread.TraditionalTread;
 
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
 public class TraditionalThread {
-    public static void main(String[] args) {
-        Thread thread1 = new Thread() {
+
+    @Test
+    public void testThread() {
+        Thread thread = new Thread() {
             @Override
             public void run() {
-                loop("thread 1");
+                loop("thread");
             }
         };
-        thread1.start();
+        thread.start();
+    }
 
+    @Test
+    public void testRunnable() {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 loop("runnable");
             }
         };
-        Thread thread2 = new Thread(runnable);
-        thread2.start();
-
-        Runnable runnable2 = ()-> loop("runnable 2");
-        Thread thread3 = new Thread(runnable2);
-        thread3.start();
-
-
-        new Thread(runnable) {
-            @Override
-            public void run() {
-                loop("thread 4");
-            }
-        }.start();
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 
-    public static void loop(String name) {
-        while(true) {
-            try {
-//                        Thread.sleep(1000);
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    @Test
+    public void testLambda() {
+        Runnable runnable = ()-> loop("runnable Lambda");
+        new Thread(runnable).start();
+    }
+
+    private void loop(String name) {
+        for(int i = 0; i < 20; i++) {
             System.out.println(name + "   " + Thread.currentThread().getName());
         }
+    }
+
+    private void sleep(int secs) {
+        try {
+            TimeUnit.SECONDS.sleep(secs);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void testCallable() throws ExecutionException, InterruptedException {
+        Callable<Integer> task = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return (new Random()).nextInt(100);
+            }
+        };
+
+        FutureTask<Integer> futureTask = new FutureTask<>(task);
+
+        // This line will keep waiting until interrupt because before task run,
+        // the futureTask.get() has nothing but wait
+//        System.out.println(futureTask.get());
+
+        futureTask.run();
+
+        System.out.println(futureTask.get());
     }
 }
