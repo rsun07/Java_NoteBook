4483813db1885b74a7a42a0395877dae43dac077
Xiaoming Sun <rsun2014@gmail.com>
2018-03-18 16:22:33 -0700

    Char Stream Output FileWrite

--- src/test/java/basic/io/char_stream/FileWriterTest.java
+++ src/test/java/basic/io/char_stream/FileWriterTest.java
@@ -1,4 +1,63 @@
 package basic.io.char_stream;
 
+import basic.io.Utils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class FileWriterTest {
+
+    private static File file;
+    private static Writer writer;
+
+    @BeforeClass
+    public static void init() {
+        File fileInit = new File(Utils.FILE_PATH + Utils.OUTPUT_FILE_NAME);
+        if (fileInit.exists()) {
+            file = fileInit;
+        }
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        writer = new FileWriter(file);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        writer.close();
+    }
+
+    @Test
+    public void testWriteCharArray() throws IOException {
+        String output = "write by Byte Array";
+        char[] data = output.toCharArray();
+        writer.write(data);
+    }
+
+    @Test
+    public void testWriteString() throws IOException {
+        String output = "write by String";
+        writer.write(output);
+    }
+
+    @Test
+    public void testWritePartialStr() throws IOException {
+        String output = "----- write by Byte Array";
+        writer.write(output, 3, 12);
+    }
+
+    @Test
+    public void testAppend() throws IOException {
+        String output = "\n\rappend this";
+        // need a input type CharSequence
+        // String implements CharSequence Interface
+        writer.append(output);
+    }
 }
