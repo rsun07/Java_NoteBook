ea7f6fc4e13783fb8e792234538c11bf347eee44
Xiaoming Sun <rsun2014@gmail.com>
2018-03-19 15:02:59 -0700

    System and finalize()

---
+++ src/main/java/basic/lang/system_and_runtime/MyStringWithFinalizer.java
@@ -0,0 +1,19 @@
+package basic.lang.system_and_runtime;
+
+class MyStringWithFinalizer  {
+    private String string;
+
+    MyStringWithFinalizer(String string) {
+        this.string = string;
+    }
+
+    MyStringWithFinalizer append(Object appendStr) {
+        return new MyStringWithFinalizer(this.string + appendStr);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("My String " + this.string + " is finalizing!");
+        super.finalize();
+    }
+}
