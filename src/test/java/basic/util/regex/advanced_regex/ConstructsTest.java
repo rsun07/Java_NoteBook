aefb8b2c471abcd54030c3de25afe3f822cbf98f
Xiaoming Sun <rsun2014@gmail.com>
2018-03-23 11:23:41 -0700

    Non capture grouping

--- src/test/java/basic/util/regex/advanced_regex/ConstructsTest.java
+++ src/test/java/basic/util/regex/advanced_regex/ConstructsTest.java
@@ -18,4 +18,33 @@ public class ConstructsTest {
 
         Assert.assertEquals("18", matcher.group("twodigits"));
     }
+
+    @Test
+    public void testNonCapturingGroup() {
+        Pattern pattern = Pattern.compile("(\\w*)(?:\\d{2})(\\w*)");
+        String input = "a18cc8eee";
+        Matcher matcher = pattern.matcher(input);
+
+        // must run matches() function before run group() function
+        Assert.assertTrue(matcher.matches());
+
+        Assert.assertEquals("a18cc8eee", matcher.group(0));
+        Assert.assertEquals("a", matcher.group(1));
+        Assert.assertEquals("cc8eee", matcher.group(2));
+    }
+
+    @Test
+    public void testNonCapturingGroupMultiMatches() {
+        Pattern pattern = Pattern.compile("(\\w*)(?:\\d{2})(\\w*)");
+        String input = "a18cc18eee";
+        Matcher matcher = pattern.matcher(input);
+
+        // must run matches() function before run group() function
+        Assert.assertTrue(matcher.matches());
+
+        // matches the last group
+        Assert.assertEquals("a18cc18eee", matcher.group(0));
+        Assert.assertEquals("a18cc", matcher.group(1));
+        Assert.assertEquals("eee", matcher.group(2));
+    }
 }
