4cc3af2d5f22e5a9baa10bb24ba0a493fc7011b7
Xiaoming Sun <rsun2014@gmail.com>
2018-03-31 23:14:37 -0700

    File appender test

--- log4j/src/test/java/pers/xiaoming/notebook/FileAppenderTest.java
+++ log4j/src/test/java/pers/xiaoming/notebook/FileAppenderTest.java
@@ -1,4 +1,22 @@
 package pers.xiaoming.notebook;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import pers.xiaoming.notebook.utils.PathUtils;
+
 public class FileAppenderTest {
+    private static Logger logger = Logger.getLogger(Log4jHelloworld.class);
+    @BeforeClass
+    public static void setup() {
+        PropertyConfigurator.configure(PathUtils.TEST_RESOURCE_DIR  + "/fileappender.properties");
+    }
+
+    @Ignore("Demo test")
+    @Test
+    public void test() {
+        logger.debug("debug log");
+    }
 }
