317e449272061ce3e475263d35470026e4f3d0c0
Xiaoming Sun <rsun2014@gmail.com>
2018-04-02 20:20:38 -0700

    add db schema for jdbc

--- jdbc/src/main/java/pers/xiaoming/notebook/HelloWorld.java
+++ jdbc/src/main/java/pers/xiaoming/notebook/HelloWorld.java
@@ -1,9 +1,10 @@
-package pers.xiaoming.notebook.helloworld;
+package pers.xiaoming.notebook;
 
 import java.io.File;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class HelloWorld {
@@ -27,6 +28,16 @@ public class HelloWorld {
             Connection conn = DriverManager.getConnection(url, user, password);
 
             System.out.println(conn);
+
+            Statement stmt1 = conn.createStatement();
+
+            System.out.println(stmt1);
+
+            Statement stmt2 = conn.createStatement();
+
+            System.out.println(stmt2);
+
+            conn.close();
 
         } catch (Exception e) {
             e.printStackTrace();
