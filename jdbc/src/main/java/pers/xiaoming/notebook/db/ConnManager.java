895c81814f1096c79a483854ba8e0f1169ab7782
Xiaoming Sun <rsun2014@gmail.com>
2018-04-04 19:35:04 -0700

    Refactor ConnManager

--- jdbc/src/main/java/pers/xiaoming/notebook/db/ConnManager.java
+++ jdbc/src/main/java/pers/xiaoming/notebook/db/ConnManager.java
@@ -12,20 +12,35 @@ public class ConnManager {
     private static final String CONFIG_PATH = "." + File.separator + "src" + File.separator + "main"
             + File.separator + "resources" + File.separator;
 
-    public static Connection getConn() throws IOException, SQLException {
+    private static String url;
+    private static Properties props;
+
+    private ConnManager() throws IOException {
+        FileReader reader = new FileReader(CONFIG_PATH + "/mysql.properties");
+        Properties properties = new Properties();
+        properties.load(reader);
+
+        url = properties.getProperty("url");
+
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+
+        props = new Properties();
+        props.setProperty("user", user);
+        props.setProperty("password", password);
+
+        reader.close();
+    }
+
+    public static Connection getConn() throws SQLException, IOException {
         try {
-            FileReader reader = new FileReader(CONFIG_PATH + "/mysql.properties");
-            Properties properties = new Properties();
-            properties.load(reader);
-
-            reader.close();
-
-            String url = properties.getProperty("url");
-            String user = properties.getProperty("user");
-            String password = properties.getProperty("password");
-
-            return DriverManager.getConnection(url, user, password);
-
+            if (url == null || props == null) {
+                synchronized (ConnManager.class) {
+                    new ConnManager();
+                }
+            }
+            // return DriverManager.getConnection(url, user, password);
+            return DriverManager.getConnection(url, props);
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
