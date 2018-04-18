8b630032304a23ca506627fef05d1b6521b3c175
Xiaoming Sun <rsun2014@gmail.com>
2018-04-18 15:11:55 -0700

    ReadWriteLock Test

---
+++ java/src/test/java/pers/xiaoming/notebook/concurrent/synchronized_and_lock/read_write_lock/ReadWriteLockTest.java
@@ -0,0 +1,54 @@
+package pers.xiaoming.notebook.concurrent.synchronized_and_lock.read_write_lock;
+
+import org.junit.Test;
+import pers.xiaoming.notebook.concurrent.util.ThreadSleep;
+
+public class ReadWriteLockTest {
+    private RWLockDemo testClass = new RWLockDemo();
+
+    /*
+    // no wait time
+        Thread[main,5,main] is accessing READ
+        Thread[Thread-0,5,main] is accessing READ
+     */
+    @Test
+    public void testTwoRead() {
+        new Thread(testClass::read).start();
+        testClass.read();
+    }
+
+    /*
+        Thread[main,5,main] is accessing WRITE
+        // wait 1s
+        Thread[Thread-0,5,main] is accessing WRITE
+    */
+    @Test
+    public void testTwoWrite() {
+        new Thread(testClass::write).start();
+        testClass.write();
+    }
+
+    /*
+        Thread[Thread-0,5,main] is accessing READ
+        // wait 1s
+        Thread[main,5,main] is accessing WRITE
+    */
+    @Test
+    public void testReadThenWrite() {
+        new Thread(testClass::read).start();
+        ThreadSleep.sleep(1);
+        testClass.write();
+    }
+
+    /*
+        Thread[Thread-0,5,main] is accessing WRITE
+        // wait 1s
+        Thread[main,5,main] is accessing READ
+    */
+    @Test
+    public void testWriteThenRead() {
+        new Thread(testClass::write).start();
+        ThreadSleep.sleep(1);
+        testClass.read();
+    }
+}
