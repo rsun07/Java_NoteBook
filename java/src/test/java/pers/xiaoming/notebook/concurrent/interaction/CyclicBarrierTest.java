5c77c1d6a0a8e6bb9d49219d8c9efd633042c484
Xiaoming Sun <rsun2014@gmail.com>
2018-04-20 15:52:55 -0700

    Test Cyclic Barrier

---
+++ java/src/test/java/pers/xiaoming/notebook/concurrent/interaction/CyclicBarrierTest.java
@@ -0,0 +1,61 @@
+package pers.xiaoming.notebook.concurrent.interaction;
+
+import org.junit.Test;
+import pers.xiaoming.notebook.concurrent.util.ThreadSleep;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierTest {
+    private CyclicBarrier barrier = new CyclicBarrier(5);
+
+    /*
+
+    Thread[Thread-0,5,main] is executing.
+    Thread[Thread-1,5,main] is executing.
+    Thread[Thread-2,5,main] is executing.
+
+    // wait 1s
+    Thread[Thread-3,5,main] is executing.
+
+     */
+    @Test
+    public void test() {
+        new Thread(this::faster).start();
+        new Thread(this::faster).start();
+        new Thread(this::faster).start();
+        new Thread(this::slower).start();
+
+        // this is for main thread wait all thread done.
+        // but the difference between DownCountLatch and CyclicBarrier is
+        // DownCountLatch is mainly for one thread wait all others
+        // CyclicBarrier is for all thread to wait for an event
+        barrierWait();
+    }
+
+    private void faster() {
+        System.out.println(Thread.currentThread() + " is executing.");
+
+        barrierWait();
+    }
+
+    private void slower() {
+        ThreadSleep.sleep(500);
+
+        System.out.println(Thread.currentThread() + " is executing.");
+
+        barrierWait();
+    }
+
+    private void barrierWait() {
+        try {
+            barrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
