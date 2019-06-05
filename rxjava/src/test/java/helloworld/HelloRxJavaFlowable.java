aba55e1cec394ddf7a8d474b54326cfa46b49231
Xiaoming Sun <rsun2014@gmail.com>
2019-06-05 14:25:21 -0700

    Use of Consumer in Flowable

--- rxjava/src/test/java/helloworld/HelloRxJavaFlowable.java
+++ rxjava/src/test/java/helloworld/HelloRxJavaFlowable.java
@@ -5,6 +5,8 @@ import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.FlowableSubscriber;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.reactivestreams.Subscriber;
@@ -20,6 +22,7 @@ public class HelloRxJavaFlowable {
         FlowableSubscriber<String> subscriber = new FlowableSubscriber<String>() {
             @Override
             public void onSubscribe(Subscription subscription) {
+                log.info("Instance of <{}> subscribes us", subscription.getClass());
                 subscription.request(10L);
             }
 
@@ -50,6 +53,82 @@ public class HelloRxJavaFlowable {
             }
         }, BackpressureStrategy.BUFFER);
 
+        // Link them together
         flowable.subscribe(subscriber);
+    }
+
+    // If only use onNext(), could use Consumer to replace the completed implementation of FlowableSubscriber
+    @Test
+    public void simplifiedFlowableDemo() {
+        StringBuilder sb = new StringBuilder();
+
+        Flowable<String> flowable = Flowable.create(new FlowableOnSubscribe<String>() {
+            @Override
+            public void subscribe(FlowableEmitter<String> flowableEmitter) throws Exception {
+                flowableEmitter.onNext("a");
+                flowableEmitter.onNext("b");
+                flowableEmitter.onComplete();
+                log.info("Result string is: {}" ,sb.toString());
+            }
+        }, BackpressureStrategy.BUFFER);
+
+        flowable.subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                sb.append(s);
+                log.info("append {}, new the sb is {}", s, sb.toString());
+            }
+        });
+    }
+
+    @Test
+    public void ConsumerDemo() {
+        StringBuilder sb = new StringBuilder();
+
+        Flowable<String> flowable = Flowable.create(new FlowableOnSubscribe<String>() {
+            @Override
+            public void subscribe(FlowableEmitter<String> flowableEmitter) throws Exception {
+                flowableEmitter.onNext("a");
+                flowableEmitter.onNext("b");
+                flowableEmitter.onComplete();
+                log.info("Result string is: {}" ,sb.toString());
+            }
+        }, BackpressureStrategy.BUFFER);
+
+        flowable.subscribe(
+                // equivalent to onNext()
+                new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        sb.append(s);
+                        log.info("append {}, new the sb is {}", s, sb.toString());
+                    }
+                },
+
+                // equivalent to onError()
+                new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        log.error(throwable.getLocalizedMessage());
+                    }
+                },
+
+                // equivalent to onComplete()
+                new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        log.info("subscriber completed!");
+                    }
+                },
+
+                // equivalent to onSubscribe()
+                new Consumer<Subscription>() {
+                    @Override
+                    public void accept(Subscription subscription) throws Exception {
+                        log.info("Instance of <{}> subscribes us", subscription.getClass());
+                        subscription.request(10L);
+                    }
+                }
+        );
     }
 }
